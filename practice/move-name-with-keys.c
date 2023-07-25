2e5f8ddd78ef12ccf9863822a522fdf67f080e83
Himanshu Raj Aman <himanshu.aman@samarth.ac.in>
2023-07-25 22:08:05 +0530

    move-name-with-keys

--- practice/move-name-with-keys.c
+++ practice/move-name-with-keys.c
@@ -1,10 +1,47 @@
 #include<stdio.h>
 #include<windows.h>
+#include <stdbool.h>
 
-int main(int argc, char const *argv[])
+void gotoxy(int x, int y)
 {
     COORD c;
+    c.X = x;
+    c.Y = y;
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
-    printf("Hello");
+}
+int main(int argc, char const *argv[])
+{
+    int x = 0, y = 0;
+    while (true)
+    {
+
+        char k = getchar();
+
+        switch (k)
+        {
+        case 'a':
+            x--;
+            break;
+        case 'd':
+            x++;
+            break;
+
+        case 'w':
+            y--;
+            break;
+        case 's':
+            y++;
+            break;
+        case 27:
+            exit(0);
+            break;
+        }
+
+        system("cls");
+        gotoxy(x, y);
+        printf("Hello");
+    }
+
+    getchar();
     return 0;
 }
