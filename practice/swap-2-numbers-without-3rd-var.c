de1dda9d41ffe44d3bc721eb70cf782d00ad6ab6
Himanshu Raj Aman <himanshu.aman@samarth.ac.in>
2023-07-25 20:13:56 +0530

    swap 2 numbers without using 3rd variable

--- practice/swap-2-numbers-without-3rd-var.c
+++ practice/swap-2-numbers-without-3rd-var.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
-void swap(int *, int *);
+void swapWithoutThirdVar(int *, int *);
 
 int main(int argc, char const *argv[])
 {
-
     int x = 0, y = 0;
 
     printf("Enter 2 numbers to SWAP: (x y)\n");
@@ -12,15 +11,20 @@ int main(int argc, char const *argv[])
 
     printf("Two numbers were: (x: %d), (y: %d)\n", x, y);
     printf("Performing Swapping\n");
-    swap(&x, &y);
+    swapWithoutThirdVar(&x, &y);
     printf("Two numbers after swapping are: (x: %d), (y: %d)", x, y);
 
     return 0;
 }
 
-void swap(int *a, int *b)
+void swapWithoutThirdVar(int *a, int *b)
 {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+    // a = 10, b = 20
+    // a = 20, b = 10;
+    //  a = 30 -> (a+b) -> (10+20) = 30
+    //  b = 10 -> (a-b) -> (30-20) = 10
+    //  a = 20 -> (a-b) -> (30-10) = 20
+    *a = *a + *b;
+    *b = *a - *b;
+    *a = *a - *b;
 }
