4ca652c767a8d1aa9638b0425f93626ad1328c55
Himanshu Raj Aman <himanshu.aman@samarth.ac.in>
2023-07-25 11:13:37 +0530

    Get only integer from user input

---
+++ practice/get-in-only-from-input.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+long long int getIntegerOnly(void);
+
+int main(int argc, char const *argv[])
+{
+    printf("Enter integer only: ");
+
+    long long int x = getIntegerOnly();
+
+    printf("Integer part was %lld", x);
+    return 0;
+}
+
+long long int getIntegerOnly(void)
+{
+    long long int x = 0;
+    char ch;
+
+    do
+    {
+        ch = getchar();
+
+        if (ch >= 48 && ch <= 57)
+        {
+            printf("%d\n", ch);
+            x = (x * 10) + (ch - 48);
+        }
+        else if (ch == '\n')
+        {
+            break;
+        }
+
+    } while (true);
+    return x;
+}
